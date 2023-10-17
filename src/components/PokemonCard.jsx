c0f208c1b84b479c6aba02ac347741ee33796ade
CharlesCodee <charles.boutin.chance@gmail.com>
2023-10-17 16:14:55 +0200

    Modif quetes 3

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,17 +1,22 @@
 
 
-function PokemonCard() {
+function PokemonCard(props) {
 
-    const pokemon = pokemonList[0];
+  console.log(props);
+
+  let pokemon = props.pokemon;
+
+  console.log(props);
 
     return(
 
-        pokemon && pokemon.imgSrc ?(
-            <figure>
+        pokemon.name && pokemon.imgSrc ?(
+        <figure>
             <img src={pokemon.imgSrc} alt={pokemon.name} />
             <figcaption>{pokemon.name}</figcaption>
         </figure>
-        ):
+        )
+        :
         
         <div>
             <p>???</p>
@@ -24,15 +29,6 @@ function PokemonCard() {
   
   }
 
-  const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
+  
 
   export default PokemonCard;
